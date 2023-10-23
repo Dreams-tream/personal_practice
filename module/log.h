dc8705f911c94febf900766b66c0f2a505a16d03
Yang Cuilang <yangcuilang@gmail.com>
2023-10-23 23:20:30 +0800

    Remove log level on screen
    去掉每次打印前的等级提示
    
    Change-Id: I7ec1a217aa1e289e5ccffd7671221ae91e30eb8f

--- module/log.h
+++ module/log.h
@@ -17,7 +17,7 @@ void va_printf(const char* fmt, ...);
 void va_exec_cmd(const char* fmt, ...);
 
 #define module_exec_cmd(fmt,...) do{\
-	va_printf("CMD[%s,%d]"fmt"\n",__FUNCTION__,__LINE__,##__VA_ARGS__);\
+	va_printf("[%s,%d]"fmt"\n",__FUNCTION__,__LINE__,##__VA_ARGS__);\
 	va_exec_cmd(fmt,##__VA_ARGS__);\
 }while(0)
 
@@ -26,27 +26,23 @@ void va_exec_cmd(const char* fmt, ...);
 #define LOG_ERR(fmt, ...) do{\
 	if ( dbg_level >= LOG_LEVEL_ERROR )\
 	{\
-		va_printf("LOG_ERR[%s,%d]" fmt "\n",__func__,__LINE__,##__VA_ARGS__);\
+		va_printf("[%s,%d]" fmt "\n",__func__,__LINE__,##__VA_ARGS__);\
 	}\
 }while(0)
 
 #define LOG_WARN(fmt,arg...) do{\
 	if ( dbg_level >= LOG_LEVEL_WARN )\
-		va_printf("LOG_WARN[%s,%d]"fmt"\n",__func__,__LINE__,##arg);\
+		va_printf("[%s,%d]"fmt"\n",__func__,__LINE__,##arg);\
 }while(0)
 
 #define LOG_INFO(fmt,arg...) do{\
 	if ( dbg_level >= LOG_LEVEL_INFO )\
-	va_printf("LOG_INFO[%s,%d]"fmt"\n",__func__,__LINE__,##arg);\
+	va_printf("[%s,%d]"fmt"\n",__func__,__LINE__,##arg);\
 }while(0)
 
 #define LOG_DEBUG(fmt,arg...) do{\
 	if ( dbg_level >= LOG_LEVEL_DEBUG )\
-		va_printf("LOG_DEBUG[%s,%d]"fmt"\n",__func__,__LINE__,##arg);\
-}while(0)
-
-#define CMD(cmd) do{\
-	printf("CMD[%s,%d]%s\n",__func__,__LINE__,cmd);\
+		va_printf("[%s,%d]"fmt"\n",__func__,__LINE__,##arg);\
 }while(0)
 
 char* LogLevelToStr(int dbg_level);
